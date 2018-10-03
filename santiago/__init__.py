f5efdee450fcdc185c06f8cd9ecae2ac67484c3b
Randy Brown <randy@beingbrown.net>
2018-10-03 00:39:29 -0500

    adds a domains request
    because domains and subdomains is the most immediate need, it is the first
    module that santiago will build out. In this case, fetching all domains is
    required to be able to get a single subdomain. This should be factored out into
    its own class, with just requests acting as a shim on the requests
    library--potentially itself removed for something else.

--- santiago/__init__.py
+++ santiago/__init__.py
@@ -3,5 +3,8 @@ BASE_URI = 'https://api.digitalocean.com/v2/'
 def request(endpoint, params={}, method="get"):
     return getattr(requests, method)(BASE_URI + endpoint, params=params)
 
+def all_domains():
+    return getattr(request("/domains/"), "domains", None)
+
 def main():
     return "Hello World"
