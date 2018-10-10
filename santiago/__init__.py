3ea43fdf06235f884b2cc1e7fad304b21f1fdc68
Randy Brown <randy@beingbrown.net>
2018-10-10 18:06:11 -0500

    maps the all_domains to a rough behavior of dopy
    dopy returns only the domains portion of the response at the all_domains
    level--there may be opportunities to get the rest of the response object at a
    lower level.

--- santiago/__init__.py
+++ santiago/__init__.py
@@ -4,7 +4,7 @@ def request(endpoint, headers={}, params={}, method="get"):
     return getattr(requests, method)(BASE_URI + endpoint, headers=headers, params=params)
 
 def all_domains(api_key=None):
-    return getattr(request("/domains/", {'Content-Type': 'application/json', 'Authorization': "Bearer %s" % api_key}), "domains", None)
+    return request("/domains/", {'Content-Type': 'application/json', 'Authorization': "Bearer %s" % api_key}).json()['domains']
 
 def main():
     return "Hello World"
