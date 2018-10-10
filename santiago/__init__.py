4534cae765d4606a336fea1257eb7e208053f930
Randy Brown <randy@beingbrown.net>
2018-10-09 22:41:10 -0500

    adds an allowance for a bearer token
    this may need to be pushed down further, as these headers will be required for
    every request. There's probably an option here to build out an object
    internally or something that can be leveraged to provide the headers.

--- santiago/__init__.py
+++ santiago/__init__.py
@@ -3,8 +3,8 @@ BASE_URI = 'https://api.digitalocean.com/v2/'
 def request(endpoint, headers={}, params={}, method="get"):
     return getattr(requests, method)(BASE_URI + endpoint, headers=headers, params=params)
 
-def all_domains():
-    return getattr(request("/domains/"), "domains", None)
+def all_domains(api_key=None):
+    return getattr(request("/domains/", {'Content-Type': 'application/json', 'Authorization': "Bearer %s" % api_key}), "domains", None)
 
 def main():
     return "Hello World"
