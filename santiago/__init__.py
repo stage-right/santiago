ad6a6443bd34fe6463f7c634d8434605ee1e8e88
Randy Brown <randy@beingbrown.net>
2018-10-11 19:37:55 -0500

    extracts headers into its own method
    because the headers have to be built the same way for every method, they are
    packed into their own method. This can likely be extracted a bit more--or
    decorated--in order to get a better experience and reduce duplication. The only
    difference between droplets and domains is the endpoint--and consequently the
    key to return.

--- santiago/__init__.py
+++ santiago/__init__.py
@@ -5,8 +5,14 @@ BASE_URI = 'https://api.digitalocean.com/v2/'
 def request(endpoint, headers={}, params={}, method="get"):
     return getattr(requests, method)(BASE_URI + endpoint, headers=headers, params=params)
 
+def headers(api_key=None):
+    return {'Content-Type': 'application/json', 'Authorization': "Bearer %s" % api_key}
+
 def all_domains(api_key=None):
-    return request("/domains/", {'Content-Type': 'application/json', 'Authorization': "Bearer %s" % api_key}).json()['domains']
+    return request("/domains/", headers(api_key)).json()['domains']
+
+def all_droplets(api_key=None):
+    return request("/droplets/", headers(api_key)).json()['droplets']
 
 def main():
     return "Hello World"
