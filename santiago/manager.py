c7250521950494efa835e76f1e1d1d21beee6c11
Randy Brown <randy@beingbrown.net>
2018-11-08 22:44:08 -0600

    passes the api_key on to santiago methods
    this allows for transparent mapping of DoManager to santiago, so that it can be
    used in place of dopy. However, there needs to be some reconstruction as
    santiago will want to accept dictionaries, but dopy accepts an arg tuple which
    is assigned. There may be some opportunities to leverage and break up kwargs to
    facilitate this.

--- santiago/manager.py
+++ santiago/manager.py
@@ -3,4 +3,8 @@ import santiago
 class DoManager(object):
     def __init__(self, client_id, api_key):
         self.api_key=api_key
-    
+    def __getattr__(self, name):
+        if hasattr(santiago, name) and hasattr(getattr(santiago, name), "__call__"):
+            return lambda *args, **kwargs: getattr(santiago, name)(api_key=self.api_key, *args, **kwargs)
+        else:
+            raise AttributeError
