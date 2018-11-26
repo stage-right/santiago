dbe8af344095a6f12e73c2dc85772fc4e20089b2
Randy Brown <randy@beingbrown.net>
2018-11-25 19:49:11 -0600

    make santiago invokable from the terminal
    the addition of the entrypoint script should add santiago to the python bin. It
    may need to give a context, so this might cause failure.

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ setup(
     name='santiago',
     version='0.1.0dev',
     packages=['santiago',],
+    entry_points={
+        'console_scripts': ['santiago = santiago/util:main']
+    },
     license='License :: OSI Approved :: MIT License'
 )
